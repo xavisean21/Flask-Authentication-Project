546fcbcbcaa1761aeebe1326cd0c5bf2498eb725
chukumah xavier <106495107+xavisean21@users.noreply.github.com>
2023-07-09 20:40:21 +0100

    This project demonstrates a Flask web application that provides user authentication, registration, and access to secret content. It utilizes various Flask extensions and libraries including Flask, Flask SQLAlchemy, Flask Login, and Werkzeug.
    The application allows users to register with their email, securely stores their hashed passwords, and provides login functionality. Authenticated users can access a secrets page where their name is displayed. Additionally, authenticated users can download a cheat sheet file from the static files directory.
    
    The project demonstrates how to create database models using SQLAlchemy, implement user authentication with login and registration forms, handle password hashing and verification, and utilize Flask Login to manage user sessions and access restrictions.
    
    To run the application, make sure to install the required dependencies and configure the database URI in the code. You can then start the Flask development server to test and interact with the application.

---
+++ main.py
@@ -0,0 +1,101 @@
+import werkzeug
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_login import LoginManager
+app = Flask(__name__)
+
+app.secret_key = "59d4feae7977a8e5c70a34cbbca515f9375b95ac9642fcf440592e5ae5e8437b"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+##CREATE TABLE IN DB
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+#Line below only required once, when creating DB. 
+# db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/')
+def home():
+    return render_template("index.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method  == "POST":
+        if User.query.filter_by(email=request.form.get('email')).first():
+            # User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
+        new_user = User(
+            id = request.form.get("id"),
+            email = request.form.get("email"),
+            password= werkzeug.security.generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8),
+            name = request.form.get("name")
+        )
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("secrets"))
+    return render_template("register.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # Find user by email entered.
+        user = User.query.filter_by(email=email).first()
+        # Check if typed email exist in the user database.
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+            # Password incorrect
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+            # Email exists and password correct
+        else:
+            login_user(user)
+            return redirect(url_for('secrets'))
+
+    return render_template("login.html", logged_in=current_user.is_authenticated)
+
+
+@app.route('/secrets')
+@login_required
+def secrets():
+    print(current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
+
+
+@app.route('/logout')
+def logout():
+    pass
+
+
+@app.route('/download')
+@login_required
+def download():
+    return send_from_directory('static', filename="files/cheat_sheet.pdf")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
